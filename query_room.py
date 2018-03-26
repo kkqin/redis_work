bd308a20e27c48e747838262d3dc9e8bf18416ad
kkqin <xolaoqin@gmail.com>
2018-03-26 12:12:36 +0800

    alert

--- query_room.py
+++ query_room.py
@@ -1,21 +1,28 @@
 import redis
 
-r = redis.StrictRedis(host='127.0.0.1', port=6379, db=0, password=123456, socket_timeout=None, connection_pool=None, charset='utf-8', errors='strict', unix_socket_path=None)
+r = redis.StrictRedis(host='127.0.0.1', port=6379, db=0, password='', socket_timeout=None, connection_pool=None, charset='utf-8', errors='strict', unix_socket_path=None)
 
-lua = """
-        local cur = 0 
-        local res = {} 
-        local need = {} 
-        repeat res = redis.call('SCAN', cur, 'match', KEYS[1], 'COUNT', 10) cur = tonumber(res[1]) 
-        if next(res) ~= nil then 
-            table.insert(need , res[2][1]) 
-        end
-        until cur == 0 
-        
-        return need 
-    """
+cur = 0
+pass_one = 1
 
-script = r.register_script(lua)
-k = "room:*:info"
-res = script(keys=[k], args=[]) 
-print("room:*:info size is: %d." % ( len(res) ) )
+data = []
+while (cur or pass_one):
+    pass_one = 0
+
+    lua = """
+            local res = {}
+            res = redis.call('SCAN', ARGV[1], 'match', KEYS[1], 'COUNT', 100) 
+            return res
+        """
+
+    script = r.register_script(lua)
+    k = "room:*:info"
+    res = script(keys=[k], args=[cur]) 
+    cur = int(res[0])
+    
+    if (len(res[1])):
+        for it in res[1]:
+            data.append(it)
+
+print(data)
+print("room:*:info size is: %d." % ( len(data) ) )
