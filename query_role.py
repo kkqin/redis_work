46edc12f9a288f80ee66b13467b39ae6a0df127b
kkqin <xolaoqin@gmail.com>
2018-03-26 12:27:51 +0800

    alert

--- query_role.py
+++ query_role.py
@@ -1,28 +1,26 @@
 import redis
 
-r = redis.StrictRedis(host='127.0.0.1', port=6379, db=0, password=123456, socket_timeout=None, connection_pool=None, charset='utf-8', errors='strict', unix_socket_path=None)
+r = redis.StrictRedis(host='127.0.0.1', port=6379, db=0, password='', socket_timeout=None, connection_pool=None, charset='utf-8', errors='strict', unix_socket_path=None)
 
-lua = """
-        local cur = 0 
-        local res = {} 
-        local need = {} 
-        repeat res = redis.call('SCAN', cur, 'match', KEYS[1], 'COUNT', 10) 
-        
-        cur = tonumber(res[1]) 
-        
-        if next(res[2]) ~= nil then 
-            for i, v in ipairs(res[2]) do
-                table.insert(need , v) 
-            end
-        end
-        until cur == 0 
-        
-        return need 
-    """
+cur = 0
+pass_one = 1
 
-script = r.register_script(lua)
-k = "role:*:info"
-res = script(keys=[k], args=[]) 
-print(res)
-print("room:*:info size is: %d." % ( len(res) ) )
+data = []
+while (cur or pass_one):
+    pass_one = 0
 
+    lua = """
+            local res = {} 
+            res = redis.call('SCAN', ARGV[1], 'match', KEYS[1], 'COUNT', 10) 
+            return res 
+        """
+    
+    script = r.register_script(lua)
+    k = "role:*:info"
+    res = script(keys=[k], args=[cur]) 
+    cur = int(res[0])
+    if (len(res[1])):
+        for it in res[1]:
+            data.append(it)
+
+print(data)
